f721d09d7ed11dd80d7f7e9b9ced944748958686
Navaneeth <navaneethkulal8@gmail.com>
2022-08-08 11:52:36 +0530

    practing2

--- practing.py
+++ practing.py
@@ -15,7 +15,7 @@ filemenu = Menu(root)
 m1 = Menu(filemenu,tearoff=0) #tearoff is the removeable  widget thing
 m1.add_command(label="save",command=myfunc)
 m1.add_command(label="save as",command=myfunc)
-m1.add_separator()
+m1.add_separator() # line bw the options
 m1.add_command(label="navi",command=myfunc)
 m1.add_command(label="print",command=myfunc)
 root.config(menu=filemenu)
